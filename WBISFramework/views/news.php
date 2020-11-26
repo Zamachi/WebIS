02105214e72fc0bb214ad3cc8cc4447fd5eb98ca
Zamachi <32929482+Zamachi@users.noreply.github.com>
2020-11-26 21:57:30 +0100

    Ispravio main

--- WBISFramework/views/news.php
+++ WBISFramework/views/news.php
@@ -8,14 +8,6 @@ use app\core\Form;
 <html>
     <main class="wrapper">
         <div class="middle">
-            <!-- <form action="loginProcess" method="POST" class="login-form">
-            <label for="mail" class="form-label">Enter your e-mail:</label></br>
-            <input type="email" name="mail" class="mail" placeholder="email"></br>
-            <label for="password" class="form-label">Enter your password:</label></br>
-            <input type="password" name="password" class="password" placeholder="password"></br>
-            <button type="submit" class="login-button">Send</button></br>
-            </form> -->
-
             <?php echo Form::beginForm("newsProcess","post","news-form") ?>
                 <?php echo Form::field($model,"title","text") ?>
                 <?php echo Form::field($model,"description","") ?> <!-- drugi argument treba da bude za textArea -->
