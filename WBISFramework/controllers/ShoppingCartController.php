1c04465a0e34381ffbb45355a8ec8d41c50b4842
Zamachi <32929482+Zamachi@users.noreply.github.com>
2020-11-29 18:01:01 +0100

    Potpuno funkcionalna shopping korpa

--- WBISFramework/controllers/ShoppingCartController.php
+++ WBISFramework/controllers/ShoppingCartController.php
@@ -7,6 +7,7 @@ use app\core\Controller;
 use app\models\CartModel;
 use app\models\CheckoutModel;
 use app\models\CodesModel;
+use app\models\UserModel;
 
 class ShoppingCartController extends Controller
 {
@@ -42,6 +43,7 @@ class ShoppingCartController extends Controller
         //var_dump($_SESSION['cart']);
         $model = new CheckoutModel();
         $codeModel = new CodesModel();
+        $userModel = new UserModel();
         if (isset($_SESSION['cart'])) {
             foreach ($_SESSION['cart'] as $item) {
                 $data['code_id'] = $item[2];
@@ -49,6 +51,8 @@ class ShoppingCartController extends Controller
                 $model->loadData($data);
                 $codeModel->updateOne('is_sold',1,'code_id',$model->code_id);
                 $model->create();
+                $userModel->updateOne('account_balance',"account_balance - ".$item[3]."",'user_id',$model->user_id);
+                $_SESSION['user']->account_balance = $userModel->one("user_id = ". $model->user_id."")['account_balance'];
                 Application::$app->session->deleteCart($model->code_id);
             }
         }
